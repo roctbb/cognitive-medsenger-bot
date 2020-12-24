9c5f6d2c4f7e3f820925cdfa1114f2c17d77d07e
roctbb <roctbb@gmail.com>
2020-12-24 16:50:58 +0300

    mvp cognitive test

--- cognitive-bot.py
+++ cognitive-bot.py
@@ -13,8 +13,65 @@ app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = db_string
 #db = SQLAlchemy(app)
 
-@app.route('/')
+@app.route('/status', methods=['POST'])
+def status():
+    data = request.json
+
+    if data['api_key'] != APP_KEY:
+        return 'invalid key'
+
+    answer = {
+        "is_tracking_data": False,
+        "supported_scenarios": ['memoryclinic'],
+        "tracked_contracts": []
+    }
+
+    return json.dumps(answer)
+
+@app.route('/init', methods=['POST'])
+def init():
+    data = request.json
+
+    if data['api_key'] != APP_KEY:
+        return 'invalid key'
+
+    send_message(data['contract_id'], "Пройдите тестирование на уровень когнитивных навыков.", "/testing", "Пройти", only_patient=True)
+    return 'ok'
+
+@app.route('/settings', methods=['GET'])
+def settings():
+    key = request.args.get('api_key', '')
+
+    if key != APP_KEY:
+        return "<strong>Некорректный ключ доступа.</strong> Свяжитесь с технической поддержкой."
+
+    return "In development"
+
+@app.route('/message', methods=['POST'])
+def save_message():
+    data = request.json
+    key = data['api_key']
+
+    if key != APP_KEY:
+        return "<strong>Некорректный ключ доступа.</strong> Свяжитесь с технической поддержкой."
+
+    return "ok"
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'waiting for the thunder!'
+
+
+@app.route('/remove', methods=['POST'])
+def remove():
+    return 'ok'
+
+@app.route('/testing')
 def index():
     return render_template('diagnostics.html')
 
+def url(to):
+    return "http://" + HOST + ":" + PORT + to
+
+app.jinja_env.globals.update(url=url)
 app.run(host=HOST, port=PORT, debug=True)
